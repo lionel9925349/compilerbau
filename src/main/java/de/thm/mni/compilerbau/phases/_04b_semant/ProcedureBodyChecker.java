ef33cc03e9bee3090e8fdeb0460152032cd745a9
CedricNdong <juniorndong901@gmail.com>
2022-01-17 01:21:33 +0100

    4b Semantik -- Anfang

--- src/main/java/de/thm/mni/compilerbau/phases/_04b_semant/ProcedureBodyChecker.java
+++ src/main/java/de/thm/mni/compilerbau/phases/_04b_semant/ProcedureBodyChecker.java
@@ -2,10 +2,11 @@ package de.thm.mni.compilerbau.phases._04b_semant;
 
 import de.thm.mni.compilerbau.absyn.*;
 import de.thm.mni.compilerbau.absyn.visitor.DoNothingVisitor;
-import de.thm.mni.compilerbau.table.ProcedureEntry;
+import de.thm.mni.compilerbau.absyn.visitor.Visitor;//
+import de.thm.mni.compilerbau.table.ProcedureEntry;//
 import de.thm.mni.compilerbau.table.SymbolTable;
-import de.thm.mni.compilerbau.table.VariableEntry;
-import de.thm.mni.compilerbau.types.ArrayType;
+import de.thm.mni.compilerbau.table.VariableEntry;//
+import de.thm.mni.compilerbau.types.ArrayType;//
 import de.thm.mni.compilerbau.types.PrimitiveType;
 import de.thm.mni.compilerbau.types.Type;
 import de.thm.mni.compilerbau.utils.NotImplemented;
@@ -20,7 +21,82 @@ import de.thm.mni.compilerbau.utils.SplError;
 public class ProcedureBodyChecker {
     public void checkProcedures(Program program, SymbolTable globalTable) {
         //TODO (assignment 4b): Check all procedure bodies for semantic errors
-
         throw new NotImplemented();
+    }
+    private class ProcedureBodyVisitor extends DoNothingVisitor {
+
+        //AssignStatement
+        @Override
+        public void visit(AssignStatement assignStatement) {
+            assignStatement.target.accept(this);
+            assignStatement.value.accept(this);
+            if( !(assignStatement.target.dataType == assignStatement.value.dataType)){
+                throw SplError.AssignmentHasDifferentTypes(assignStatement.position);
+            }else if((assignStatement.target.dataType != PrimitiveType.intType)){
+                throw SplError.AssignmentRequiresIntegers(assignStatement.position);
+            }
+        }
+        //IfStatement
+        @Override
+        public void visit(IfStatement ifStatement) {
+            ifStatement.condition.accept(this);
+            ifStatement.thenPart.accept(this);
+            if (ifStatement.elsePart !=null){
+                ifStatement.elsePart.accept(this);
+            }
+            if(!(ifStatement.condition.dataType == PrimitiveType.boolType)){
+                throw SplError.IfConditionMustBeBoolean(ifStatement.position);
+            }
+
+        }
+        //WhileStatement
+        @Override
+        public void visit(WhileStatement whileStatement) {
+            whileStatement.condition.accept(this);
+            whileStatement.body.accept(this);
+
+            if(!(whileStatement.condition.dataType == PrimitiveType.boolType)){
+                throw SplError.WhileConditionMustBeBoolean(whileStatement.position);
+            }
+
+        }
+        //ProcedureDeclaration
+        @Override
+        public void visit(ProcedureDeclaration procedureDeclaration) {
+
+
+        }
+        //BinaryExpression
+        public void visit(BinaryExpression binaryExpression) {
+
+
+        }
+        //CompoundStatement
+        @Override
+        public void visit(CompoundStatement compoundStatement) {
+            for (Statement cs : compoundStatement.statements){
+                cs.accept(this);
+            }
+        }
+        //IntLiteral
+        @Override
+        public void visit(IntLiteral intLiteral) {
+           intLiteral.dataType = PrimitiveType.intType;
+        }
+        //VariableExpression
+        @Override
+        public void visit (VariableExpression variableExpression){
+            variableExpression.variable.accept(this);
+            variableExpression.dataType = variableExpression.variable.dataType;
+
+        }
+        //Programm
+        @Override
+        public void visit(Program program) {
+            for (GlobalDeclaration gb : program.declarations){
+                gb.accept(this);
+            }
+        }
+
     }
 }
